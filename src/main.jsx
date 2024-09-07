e24c9ea96ae3eb92cf07ccece8bde6a7b5230ba2
rahulpant <rohanpant80@gmail.com>
2024-09-08 02:30:42 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App'
-// import Routing from './routing'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
        <BrowserRouter>
          <App />
         
         </BrowserRouter>
-  // </React.StrictMode>,
+   </React.StrictMode>,
 )
